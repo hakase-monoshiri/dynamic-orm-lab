cec0fc01162426dc9673254c6fca9f4ee83e9fb3
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-02-23 17:47:17 -0500

    commit tree 7daf96d68b0fea0fcbc456aac5b800dedf9b2b1e

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,16 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+    
+    self.column_names.each {|column_name| attr_accessor column_name.to_sym}
+    
+    def initialize(attributes_hash = {})
+        attributes_hash.map do |attribute_key, attribute_value|
+        self.send("#{attribute_key}=", attribute_value)
+        end
+    end
+
+
+
 
 end
